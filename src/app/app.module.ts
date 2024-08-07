d94d2559b2bff20721b2833bfceef46d4873331a
aaron-gcl-bi <aaron@gcl-bi.com>
2024-08-07 13:11:11 +0530

    Initial commit. Set up the basic header till now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { ApplicationModule } from './application/application.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApplicationModule,
+    AuthenticationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/application/application.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HeaderComponent
+  ]
+})
+export class ApplicationModule { }
