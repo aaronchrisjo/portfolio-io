857694f1bf270e4c896c27fb1f4a0ca54c58c7b7
aaron-gcl-bi <aaron@gcl-bi.com>
2024-08-09 10:25:28 +0530

    Conditionally rendered the signup button to be only shown in the homepage when no user logged in.

--- src/app/application/header/header.component.ts
+++ src/app/application/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Observable } from 'rxjs';
 import { User } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,13 @@ import { User } from '@angular/fire/auth';
 })
 export class HeaderComponent {
   isAuthenticated$: Observable<User | null>;
+  currentRoute: string = '';   
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.isAuthenticated$ = this.authService.isAuthenticated;
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
   }
 
   ngOnInit():void{}
--- src/app/application/home/home.component.ts
+++ src/app/application/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  username: string | null = null;
 
+  constructor(private auth: Auth) {}
+
+  ngOnInit(): void {
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        // The user is logged in, extract the display name
+        this.username = user.displayName ? user.displayName : user.email;
+      } else {
+        // The user is not logged in, handle accordingly
+        this.username = null;
+      }
+    });
+  }
 }
