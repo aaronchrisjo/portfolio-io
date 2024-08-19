295c123f2fd299979782084bcc731097920cf992
aaron-gcl-bi <aaron@gcl-bi.com>
2024-08-19 17:38:47 +0530

    Added pagination controls to the all-templates page

--- src/app/template/all-templates/all-templates.component.ts
+++ src/app/template/all-templates/all-templates.component.ts
@@ -3,7 +3,7 @@ import { Product } from '../../interfaces/product';
 import { ProductService } from '../../services/product.service';
 
 @Component({
-  selector: 'app-your-component',
+  selector: 'app-all-templates',
   templateUrl: './all-templates.component.html',
   styleUrls: ['./all-templates.component.css']
 })
@@ -13,18 +13,51 @@ export class AllTemplatesComponent implements OnInit {
   selectedFilter: string = 'all';
   selectedProduct: Product | null = null;
 
+  // Pagination properties
+  currentPage: number = 1;
+  itemsPerPage: number = 12;
+  totalPages: number = 0;
+
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(products => {
       this.products = products;
-      this.filterProducts(this.selectedFilter); // Apply initial filter
+      this.totalPages = Math.ceil(this.products.length / this.itemsPerPage);
+      this.updatePage();
     });
   }
 
   filterProducts(category: string): void {
     this.selectedFilter = category;
-    this.filteredProducts = category === 'all' ? this.products : this.products.filter(product => product.category === category);
+    const filtered = category === 'all' ? this.products : this.products.filter(product => product.category === category);
+    this.totalPages = Math.ceil(filtered.length / this.itemsPerPage);
+    this.updatePage();
+  }
+
+  updatePage(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.filteredProducts = this.products.slice(startIndex, endIndex);
+  }
+
+  goToPage(page: number): void {
+    this.currentPage = page;
+    this.updatePage();
+  }
+
+  goToNextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePage();
+    }
+  }
+
+  goToPreviousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePage();
+    }
   }
 
   openModal(product: Product): void {
