93231659887fbec1a005f17552eec751899bcd34
aaron-gcl-bi <aaron@gcl-bi.com>
2024-08-24 16:48:11 +0530

    Mobile device warning set .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-io';
+
+  ngOnInit():void{
+    this.checkIfMobile();
+  }
+
+  checkIfMobile(): void {
+    const isMobile = /iPhone|iPad|iPod|Android|webOS|BlackBerry|Windows Phone/i.test(navigator.userAgent) 
+      || window.innerWidth <= 800;
+      
+    if (isMobile) {
+      alert('This app is not yet developed for mobile devices. Please continue using in a PC.');
+    }
+  }
 }
