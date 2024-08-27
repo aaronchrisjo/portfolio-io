1d3178b2ca294440824929d39af9a5a3b2068433
aaron-gcl-bi <aaron@gcl-bi.com>
2024-08-27 09:38:55 +0530

    Fixed header setup successfully. Adjusted other components to add mt-# . Test out on other systems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-io';
+  constructor(private router: Router){}
+  
 
   ngOnInit():void{
     this.checkIfMobile();
+    this.router.events.subscribe((event)=>{
+      if (event instanceof NavigationEnd){
+        this.scrollTopWithOffset();
+      }
+    })
   }
 
   checkIfMobile(): void {
@@ -19,5 +27,14 @@ export class AppComponent {
     if (isMobile) {
       alert('This app is not yet developed for mobile devices. Please continue using in a PC.');
     }
+  }
+
+  scrollTop(){
+    window.scrollTo({top:0, behavior:'smooth'})
+  }
+
+  scrollTopWithOffset(){
+    const heaaderHeight = document.querySelector('app-header')?.clientHeight || 60;
+    window.scrollTo({top:heaaderHeight, behavior:'smooth'})
   }
 }
