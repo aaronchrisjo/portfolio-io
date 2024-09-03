d74d4d28596e5d417b6710f70031704a0a7caec3
aaron-gcl-bi <94833179+aaronchrisjo@users.noreply.github.com>
2024-09-03 09:58:43 +0530

    commented out the authentication login and signup routes. Authentication currently unused as it is lazyloaded as well.

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -4,8 +4,8 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'signup', component:SignupComponent},
-  {path:'login', component:LoginComponent},
+  // {path:'signup', component:SignupComponent},
+  // {path:'login', component:LoginComponent},
 ];
 
 @NgModule({
